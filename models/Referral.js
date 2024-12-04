6672c94d28d267993627f1a24354577a63c030de
PrinceKes <princeade5565@gmail.com>
2024-12-03 23:04:02 -0800

    update all

--- models/Referral.js
+++ models/Referral.js
@@ -1,15 +1,36 @@
 const mongoose = require('mongoose');
 
 const referralSchema = new mongoose.Schema({
-  referral_id: { type: String, required: true, unique: true },
-  referred_Users: [
+  username: { type: String, required: true },
+  referrals: [
     {
-      user_id: { type: String, required: true },
-      username: { type: String, required: true },
+      referredUserId: { type: String, required: true },
+      referredUsername: { type: String, required: true },
       reward: { type: Number, required: true },
     },
   ],
-}, { collection: 'referral' }); // Specify collection name explicitly
+});
 
 const Referral = mongoose.model('Referral', referralSchema);
 module.exports = Referral;
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const referralSchema = new mongoose.Schema({
+//   referral_id: { type: String, required: true, unique: true },
+//   referred_Users: [
+//     {
+//       user_id: { type: String, required: true },
+//       username: { type: String, required: true },
+//       reward: { type: Number, required: true },
+//     },
+//   ],
+// }, { collection: 'referral' }); // Specify collection name explicitly
+
+// const Referral = mongoose.model('Referral', referralSchema);
+// module.exports = Referral;
