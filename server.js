33488514951a39fd4b622e3f070d05c936823fe7
Enoch | StrongNationDev <96037694+PrinceKes@users.noreply.github.com>
2024-12-07 11:31:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -424,6 +424,28 @@ app.get('/api/referrals', async (req, res) => {
 
 
 
+//New code from the latest updating 
+app.get('/api/referrals/:referral_id', async (req, res) => {
+  const { referral_id } = req.params;
+
+  try {
+    // Find the user with the given referral_id
+    const referrer = await Referral.findOne({ referral_id });
+
+    if (!referrer) {
+      return res.status(404).json({ message: 'Referrer not found' });
+    }
+
+    // Return the referred users
+    res.status(200).json(referrer.referrals);
+  } catch (error) {
+    console.error('Error fetching referred users:', error);
+    res.status(500).json({ message: 'Error fetching referred users', error });
+  }
+});
+
+
+
 app.put('/api/referrals/:referral_id/claim', async (req, res) => {
   const { referral_id } = req.params;
 
