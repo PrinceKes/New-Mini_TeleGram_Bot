aa078723631db5b1f2efd1672b625b3bf3bb1408
Enoch | StrongNationDev <96037694+PrinceKes@users.noreply.github.com>
2024-12-07 13:52:59 +0100

    Update Referral.js

--- models/Referral.js
+++ models/Referral.js
@@ -17,25 +17,3 @@ const referralSchema = new mongoose.Schema({
 const Referral = mongoose.model('Referral', referralSchema, 'Referrals'); 
 
 module.exports = Referral;
-
-
-
-
-
-//const mongoose = require('mongoose');
-
-//const referralSchema = new mongoose.Schema({
-//  referral_id: { type: String, required: true, unique: true }, // Custom referral ID
-//  username: { type: String, required: true },
-//  referrals: [
-    //{
- //     referredUserId: { type: String, required: true },
-//      referredUsername: { type: String, required: true },
-//      reward: { type: Number, required: true },
-//      isClaimed: { type: Boolean, default: false },
-//    },
-//  ],
-//});
-
-//const Referral = mongoose.model('Referral', referralSchema);
-//module.exports = Referral;
